30d80642e56fbc88a552b43bf7252827c0c82662
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2025-04-23 22:35:38 -0300

    Adiciona notificacoes de plano alimentar em pdf e consultas geradas por email

--- nutriNow/signals.py
+++ nutriNow/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import PlanoAlimentar
+from .models import Consulta
 from utils.gerar_plano import gerar_pdf
-from utils.enviar_email import enviar_email_plano
+from utils.enviar_email import enviar_email_plano,enviar_email_consulta_confirmada
 
 @receiver(post_save,sender=PlanoAlimentar)
 def notificar_paciente_plano(sender,instance,created,**kwargs):
@@ -11,3 +12,12 @@ def notificar_paciente_plano(sender,instance,created,**kwargs):
         if pdf_path:
             paciente_email = instance.paciente.email
             enviar_email_plano(paciente_email,pdf_path)
+
+@receiver(post_save,sender=Consulta)
+def notificar_paciente_consulta(sender,instance,created,**kwargs):
+    if created:
+        paciente_email = instance.paciente.email
+        consulta_data = instance.data_consulta.strftime("%d/%m/%Y %H:%M")
+        consulta_nutricionista = instance.nutricionista.nome
+        enviar_email_consulta_confirmada(paciente_email,consulta_data,consulta_nutricionista=consulta_nutricionista)
+        print("Email enviado com sucesso!")
--- utils/enviar_email.py
+++ utils/enviar_email.py
@@ -13,3 +13,15 @@ def enviar_email_plano(paciente_email,plano_pdf_path):
 
     email.attach_file(plano_pdf_path)
     email.send(fail_silently=False)
+
+def enviar_email_consulta_confirmada(paciente_email, consulta_data, consulta_nutricionista):
+    assunto = "Confirmação de Consulta NutriNow"
+    mensagem = f"Olá! Sua consulta está confirmada para {consulta_data} com o(a) profissional: {consulta_nutricionista}."
+
+    email = EmailMessage(
+        assunto,
+        mensagem,
+        "NutriNow <Luiscmacedo84@gmail.com>",
+        [paciente_email],
+    )
+    email.send(fail_silently=False)
