3b0fd8d3635773729c8d926a84dfb22b69ce524e
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2025-05-05 09:19:15 -0300

    Adiciona signal para notificacao de nutricionista para pacientes que marcarem consulta

--- nutriNow/signals.py
+++ nutriNow/signals.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save
 from django.dispatch import receiver
 from .models import PlanoAlimentar
 from .models import Consulta
+from .models import Paciente
+from .models import Notificacao
 from utils.gerar_plano import gerar_pdf
 from utils.enviar_email import enviar_email_plano,enviar_email_consulta_confirmada
 
@@ -21,3 +23,16 @@ def notificar_paciente_consulta(sender,instance,created,**kwargs):
         consulta_nutricionista = instance.nutricionista.nome
         enviar_email_consulta_confirmada(paciente_email,consulta_data,consulta_nutricionista=consulta_nutricionista)
         print("Email enviado com sucesso!")
+
+@receiver(post_save,sender=Consulta)
+def notificar_nutricionista_consulta(sender,instance,created,**kwargs):
+    if created:
+        Notificacao.objects.create(
+            paciente=instance.paciente,
+            nutricionista=instance.nutricionista,
+            mensagem=f"{instance.paciente.nome} marcou uma nova consulta em {instance.data_consulta.strftime('%d/%m/%Y às %H:%M')}"
+        )
+
+
+
+
