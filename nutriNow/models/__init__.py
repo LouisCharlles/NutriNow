3eb8f5590687960947528a6ebeb6b799e10c7cf6
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2025-05-14 20:44:30 -0300

    Feat:Add custom UserManager for usuario

--- nutriNow/models/__init__.py
+++ nutriNow/models/__init__.py
@@ -4,3 +4,4 @@ from .usuario import Usuario
 from .consulta import Consulta
 from .plano_alimentar import PlanoAlimentar
 from .notificacao import Notificacao
+from .usuario_manager import UsuarioManager
